5653e8235ab7526f856fe3b3a726838dc3fcdbc9
Nithin Kumar M <amnithinkumar2810@gmail.com>
2025-05-12 11:40:29 +0530

    Upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practrice
+{
+    public class singleton
+    {
+        private static singleton instance;
+        private singleton()
+        {
+        }
+        public static singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new singleton();
+                }
+                return instance;
+            }
+        }
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Singleton instance called");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            singleton s1 = singleton.Instance;
+            singleton s2 = singleton.Instance;
+
+            s1.DisplayMessage();
+            s2.DisplayMessage();
+
+            Console.ReadKey();
+        }
+    }
+}
+
